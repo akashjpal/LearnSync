27634da29fa2a9f8f4dcc66ccf91d65a383df07c
palgayatrij786 <palgayatrij786@gmail.com>
2024-12-15 17:40:19 +0000

    added server db

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 // Suggested code may be subject to a license. Learn more: ~LicenseLog:1438614570.
 const express = require('express');
+const {connectDB,pool} = require("./dbConnect/db");
 const app = express();
 const port = 3001;
 
+app.get("/auth/signup",(req,res)=>{
+  const {username, email, password} = req.body();
+  console.log(username, email, password);
+  res.send("Hello World!");
+})
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
