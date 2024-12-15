9a658252417cb8b5763e4692df3c3aa0aa80a14c
palgayatrij786 <palgayatrij786@gmail.com>
2024-12-15 19:07:03 +0000

    Added signup and login routes

--- server/index.js
+++ server/index.js
@@ -1,14 +1,27 @@
 // Suggested code may be subject to a license. Learn more: ~LicenseLog:1438614570.
+// docker run -d --name my-postgres   -e POSTGRES_PASSWORD=mysecurepassword   -e POSTGRES_DB=mydatabase   -p 5432:5432 postgres
+
+// curl -X POST http://localhost:3001/auth/signup \
+// -H "Content-Type: application/json" \
+// -d '{"username":"JohnDoe", "email":"john@example.com", "password":"1234"}'
+
+// curl -X POST http://localhost:3001/auth/login \
+// -H "Content-Type: application/json" \
+// -d '{"email":"john@example.com", "password":"1234"}'
+
+
+
 const express = require('express');
-const {connectDB,pool} = require("./dbConnect/db");
+const bodyParser = require('body-parser')
 const app = express();
+const authRoutes = require("./routes/authRoutes");
+
 const port = 3001;
 
-app.get("/auth/signup",(req,res)=>{
-  const {username, email, password} = req.body();
-  console.log(username, email, password);
-  res.send("Hello World!");
-})
+// used by body parser to parse the user posted data in json format
+app.use(bodyParser.json());
+app.use("/auth",authRoutes);
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
